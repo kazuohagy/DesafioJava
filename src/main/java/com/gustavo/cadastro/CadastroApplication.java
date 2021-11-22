817e05e657bdf41abaa14e9c2818fb34e3a65b00
Gustavo Kazuo <kazucalado@gmail.com>
2021-11-22 18:12:08 -0300

    Agora vai

--- src/main/java/com/gustavo/cadastro/CadastroApplication.java
+++ src/main/java/com/gustavo/cadastro/CadastroApplication.java
@@ -3,7 +3,7 @@ package com.gustavo.cadastro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class CadastroApplication {
--- src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
+++ src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
@@ -17,39 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gustavo.cadastro.domain.Cliente;
 import com.gustavo.cadastro.repository.ClienteRepository;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping(value="/api")
-@Api(value="API REST Clientes")
+
 @CrossOrigin(origins="*")
 public class ClienteResource {
 	@Autowired
 	ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
-	@ApiOperation(value="Retorna uma lista de clientes")
+
 	public List<Cliente> listaClientes(){
 		return clienteRepository.findAll();
 	}
 	@GetMapping("/cliente/{id}")
-	@ApiOperation(value="Retorna um cliente unico")
+
 	public Optional<Cliente> listaClienteUnico(@PathVariable Integer id){
 		return clienteRepository.findById(id);
 	}
 	@PostMapping("/cliente")
-	@ApiOperation(value="Salva um cliente")
+
 	public Cliente salvarCliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	@DeleteMapping("/cliente")
-	@ApiOperation(value="Deleta um cliente")
+
 	public void deletaCliente(@RequestBody Cliente cliente) {
 		clienteRepository.delete(cliente);
 	}
 	@PutMapping("/cliente")
-	@ApiOperation(value="Atualiza um cliente")
+
 	public Cliente atualizaCliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
