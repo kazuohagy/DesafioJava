e0870f6c7e7a4846b9367c8a2683b1f5580145be
Gustavo Kazuo <kazucalado@gmail.com>
2021-11-22 11:58:55 -0300

    Put concluido

--- src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
+++ src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,8 @@ public class ClienteResource {
 	public void deletaCliente(@RequestBody Cliente cliente) {
 		clienteRepository.delete(cliente);
 	}
-	
+	@PutMapping("/cliente")
+	public Cliente atualizaCliente(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
 }
