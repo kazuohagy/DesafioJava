4aed004134f56a004316c920e138ae7fca9da0fb
Gustavo Kazuo <kazucalado@gmail.com>
2021-11-22 11:28:23 -0300

    findById funcionando

--- src/main/java/com/gustavo/cadastro/repository/ClienteRepository.java
+++ src/main/java/com/gustavo/cadastro/repository/ClienteRepository.java
@@ -1,11 +1,20 @@
 package com.gustavo.cadastro.repository;
 
+
+
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gustavo.cadastro.domain.Cliente;
 
+
+
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+	
+	Optional<Cliente> findById(Integer id);
 }
--- src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
+++ src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
@@ -1,9 +1,11 @@
 package com.gustavo.cadastro.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,17 @@ import com.gustavo.cadastro.domain.Cliente;
 import com.gustavo.cadastro.repository.ClienteRepository;
 
 @RestController
-@RequestMapping(value="api")
+@RequestMapping(value="/api")
 public class ClienteResource {
 	@Autowired
 	ClienteRepository clienteRepository;
 	
-	@GetMapping("/cliente")
+	@GetMapping("/clientes")
 	public List<Cliente> listaClientes(){
 		return clienteRepository.findAll();
+	}
+	@GetMapping("/clientes/{id}")
+	public Optional<Cliente> listaClienteUnico(@PathVariable Integer id){
+		return clienteRepository.findById(id);
 	}
 }
