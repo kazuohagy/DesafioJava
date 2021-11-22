25626d1224a3359a33af1a6429583c9a49264582
Gustavo Kazuo <kazucalado@gmail.com>
2021-11-22 13:49:59 -0300

    Projeto Finalizado

--- src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
+++ src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,29 +17,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gustavo.cadastro.domain.Cliente;
 import com.gustavo.cadastro.repository.ClienteRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Clientes")
+@CrossOrigin(origins="*")
 public class ClienteResource {
 	@Autowired
 	ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
+	@ApiOperation(value="Retorna uma lista de clientes")
 	public List<Cliente> listaClientes(){
 		return clienteRepository.findAll();
 	}
 	@GetMapping("/cliente/{id}")
+	@ApiOperation(value="Retorna um cliente unico")
 	public Optional<Cliente> listaClienteUnico(@PathVariable Integer id){
 		return clienteRepository.findById(id);
 	}
 	@PostMapping("/cliente")
+	@ApiOperation(value="Salva um cliente")
 	public Cliente salvarCliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	@DeleteMapping("/cliente")
+	@ApiOperation(value="Deleta um cliente")
 	public void deletaCliente(@RequestBody Cliente cliente) {
 		clienteRepository.delete(cliente);
 	}
 	@PutMapping("/cliente")
+	@ApiOperation(value="Atualiza um cliente")
 	public Cliente atualizaCliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
