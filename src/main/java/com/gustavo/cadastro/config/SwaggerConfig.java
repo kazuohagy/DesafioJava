d793325dfff91922b9b8108e0a5e6ae56ac5f241
Gustavo Kazuo <kazucalado@gmail.com>
2021-11-22 14:27:16 -0300

    Dependencias funcionando

--- src/main/java/com/gustavo/cadastro/config/SwaggerConfig.java
+++ src/main/java/com/gustavo/cadastro/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.gustavo.cadastro.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,17 +9,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-
 public class SwaggerConfig {
 	
 	@Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                ;
+                .select().build();
     }
 
   
