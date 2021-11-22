7529cbae74196bcdaec82562f72efa77c72894e6
Gustavo Kazuo <kazucalado@gmail.com>
2021-11-22 11:41:46 -0300

    Post feito

--- src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
+++ src/main/java/com/gustavo/cadastro/resource/ClienteResource.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +24,13 @@ public class ClienteResource {
 	public List<Cliente> listaClientes(){
 		return clienteRepository.findAll();
 	}
-	@GetMapping("/clientes/{id}")
+	@GetMapping("/cliente/{id}")
 	public Optional<Cliente> listaClienteUnico(@PathVariable Integer id){
 		return clienteRepository.findById(id);
 	}
+	@PostMapping("/cliente")
+	public Cliente salvarProduto(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
 }
